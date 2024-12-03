75113b01a702f4bb99be0372ba8b6574e7276dc0
CasualBot <sbernard199@gmail.com>
2024-12-03 14:35:41 -0600

    feat: complete day 1 c++

--- src/cpp/Advent2024/day1.cpp
+++ src/cpp/Advent2024/day1.cpp
@@ -2,13 +2,16 @@
 
 int main()
 {
-	std::cout << "Reading list to fix!" << std::endl;
+	std::cout << "Reading Chief Historian list!" << std::endl;
 	std::ifstream ListFile("../../../problem/day1/lists.txt");
 	std::vector<int> list1; 
 	std::vector<int> list2;
-	long finalValue = 0;
+	int part1Answer = 0;
+	int part2Answer = 0;
 	std::string line;
-
+	/**
+	* Part 1 logic, sort and verify
+	*/
 	while (std::getline(ListFile, line))
 	{
 		std::string input;
@@ -38,9 +41,33 @@ int main()
 	// iterat over both list vectors 
 	for (long i = 0; i < std::min(list1.size(), list2.size()); i++)
 	{
-		finalValue += std::abs(list1[i] - list2[i]);
+		part1Answer += std::abs(list1[i] - list2[i]);
 	}
-	std::cout << "Answer: " << finalValue << std::endl;
+	std::cout << "Part 1 Answer: " << part1Answer << std::endl;
+
+
+	/**
+	* Part 2, similiarity
+	*/
+	std::vector<int> similarties;
+	for (auto list1it = list1.begin(); list1it != list1.end(); list1it++)
+	{
+		int timesFound = 0;
+		for (auto list2it = list2.begin(); list2it != list2.end(); list2it++) 
+		{
+			if (*list1it == *list2it) {
+				timesFound++;
+			}
+		}
+		similarties.push_back(*list1it * timesFound);
+	}
+	for (auto it = similarties.begin(); it != similarties.end(); it++)
+	{
+		part2Answer += *it;
+	}
+
+	std::cout << "Part 2 Answer: " << part2Answer << std::endl;
+
 	std::cout << "\nPress enter to exit" << std::endl;
 	std::cin.ignore();
 	return 0;
